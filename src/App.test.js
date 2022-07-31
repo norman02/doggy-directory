2d10ff95bed5e9b37a92d686ea7684663a859463
John N. Norman <normanj777@gmail.com>
2022-07-31 10:05:35 -0400

    test fetch

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,22 @@
 import { render, screen } from "@testing-library/react";
+import mockFetch from "./mocks/mockFetch";
 import App from "./App";
 
+beforeEach(() => {
+  jest.spyOn(window, "fetch").mockImplementation(mockFetch);
+});
 
-test('renders the landing page', () => {
+afterEach(() => {
+  jest.restoreAllMocks();
+});
+test("renders the landing page", async () => {
   render(<App />);
-  
+
   expect(screen.getByRole("heading")).toHaveTextContent(/Doggy Directory/);
   expect(screen.getByRole("combobox")).toHaveDisplayValue("Select a breed");
+  expect(
+    await screen.findByRole("option", { name: "husky" })
+  ).toBeInTheDocument();
   expect(screen.getByRole("button", { name: "Search" })).toBeDisabled();
   expect(screen.getByRole("img")).toBeInTheDocument();
 });
-
